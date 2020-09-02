21fcabb76ebf6649161383d0c74220678bfec41e
David Coats <david@getsafetycone.com>
2020-09-02 12:35:32 -0700

    Add DisplayName property to GmailAuthentication

--- source/R5T.Gimpolis/Code/Classes/GmailAuthentication.cs
+++ source/R5T.Gimpolis/Code/Classes/GmailAuthentication.cs
@@ -6,6 +6,7 @@ namespace R5T.Gimpolis
     public class GmailAuthentication
     {
         public string Address { get; set; }
+        public string DisplayName { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
     }
